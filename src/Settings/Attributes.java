777ecfa7a8e10d3a4bf85a1af47c3a3ee986a7cf
Ori Roth <ori.rothh@gmail.com>
2016-11-08 18:04:11 +0200

    packaging

--- src/Settings/Attributes.java
+++ src/Settings/Attributes.java
@@ -1,6 +1,6 @@
-package gui;
+package Settings;
 
-public enum Settings {
+public enum Attributes {
   ;
   public static final int columns_count = 10;
   public static final int rows_count = 10;
--- src/Settings/Names.java
+++ src/Settings/Names.java
@@ -1,4 +1,4 @@
-package gui;
+package Settings;
 
 public enum Names {
   ;
